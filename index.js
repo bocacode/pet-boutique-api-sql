978a11eb6bc6b5cae7fc8b1005cb510e404ec402
Mia Sanchez <mia@Claudias-MacBook-Air.local>
2021-07-14 14:32:32 -0400

    added get petById

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { getCustomers, createCustomer } = require('./src/customers')
+const { getPetById } = require('./src/pets')
 
 const app = express()
 app.use(express.json())
@@ -7,8 +8,14 @@ app.use(express.json())
 // PUT ROUTES HERE...
 app.get('/customers', getCustomers)
 
+app.get('/pets/:byId', getPetById)
+
 app.post('/customers', createCustomer)
 
 app.listen(3000, () => {
   console.log('listening on http://localhost:3000...')
 })
+
+
+
+
