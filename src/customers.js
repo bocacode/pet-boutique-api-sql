f814b6d51a6523cfcf6855aa38835c9e47b88297
Todd Albert <todd@bocacode.com>
2021-07-14 10:36:00 -0400

    pulling customers from mysql

--- src/customers.js
+++ src/customers.js
@@ -1,4 +1,18 @@
+const mysql = require('mysql')
+const { dbconfig } = require('../dbconfig')
+
+const db = mysql.createConnection(dbconfig)
+
 // PUT CUSTOMER-RELATED FUNCTIONS HERE...
 exports.getCustomers = (req, res) => {
-  res.send('customers will appear here...')
+  db.connect()
+  db.query(`SELECT customers.*, pets.name, pets.type, pets.size
+    FROM customers LEFT JOIN pets ON pets.customer_id = customers.id`, (err, rows) => {
+    if (err) {
+      res.status(500).send(err)
+      return
+    }
+    res.send(rows)
+  })
+  db.end()
 }
