e882b1b3c2bbe267b4c170af208162d5fb4eee31
chrisdeleonn <cdizzel@ymail.com>
2021-07-14 15:13:37 -0400

    finished adding the delete method for the query in SQL

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express')
-const { getCustomers, createCustomer } = require('./src/customers')
+const express = require("express")
+const {
+  getCustomers,
+  createCustomer,
+  deleteCustomer,
+} = require("./src/customers")
 
 const app = express()
 app.use(express.json())
 
 // PUT ROUTES HERE...
-app.get('/customers', getCustomers)
 
-app.post('/customers', createCustomer)
+app.delete("/customers/:id", deleteCustomer)
+
+app.get("/customers", getCustomers)
+
+app.post("/customers", createCustomer)
 
 app.listen(3000, () => {
-  console.log('listening on http://localhost:3000...')
+  console.log("listening on http://localhost:3000...")
 })
