41120e357d2c676ce169e380da171ddd6148755a
Todd Albert <todd@bocacode.com>
2021-07-14 11:05:16 -0400

    need to fix query

--- src/customers.js
+++ src/customers.js
@@ -1,4 +1,24 @@
+const mysql = require('mysql')
+const { dbconfig } = require('../dbconfig')
+
+const db = mysql.createConnection(dbconfig)
+
 // PUT CUSTOMER-RELATED FUNCTIONS HERE...
 exports.createCustomer = (req, res) => {
-  res.send('created a customer...')
+  db.connect()
+  // get new customer data from req.body
+  const newCustomer = req.body
+  // query to create a customer...
+  db.query(`INSERT INTO customers VALUES
+    (null, "${newCustomer.name}", "${newCustomer.type}", "${newCustomer.size}")`,
+  (err, results) => {
+    // if error return error with status 500
+      if(err) {
+        res.status(500).send(err)
+        return
+      }
+      // otherwise return OK with status 201
+      res.status(201).send('Customer created')
+    })
+  db.end()
 }
