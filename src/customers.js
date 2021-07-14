8e408447df72201c2a48a5590c991f0e937b0cd1
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-07-14 15:51:05 -0400

    first attempt at merging getCustById

--- src/customers.js
+++ src/customers.js
@@ -1,37 +1,56 @@
+// const mysql = require('mysql')
+// const { dbconfig } = require('../dbconfig')
+
+// const db = mysql.createConnection(dbconfig)
+
+// // PUT CUSTOMER-RELATED FUNCTIONS HERE...
+// exports.getCustomers = (req, res) => {
+//   db.connect()
+//   db.query(`SELECT customers.*, pets.name, pets.type, pets.size
+//     FROM customers LEFT JOIN pets ON pets.customer_id = customers.id`, (err, rows) => {
+//     if (err) {
+//       res.status(500).send(err)
+//       return
+//     }
+//     res.send(rows)
+//   })
+//   db.end()
+// }
+
 const mysql = require('mysql')
 const { dbconfig } = require('../dbconfig')
 
 const db = mysql.createConnection(dbconfig)
-
-// PUT CUSTOMER-RELATED FUNCTIONS HERE...
-exports.getCustomers = (req, res) => {
-  db.connect()
-  db.query(`SELECT customers.*, pets.name, pets.type, pets.size
-    FROM customers LEFT JOIN pets ON pets.customer_id = customers.id`, (err, rows) => {
-    if (err) {
-      res.status(500).send(err)
-      return
-    }
-    res.send(rows)
-  })
-  db.end()
-}
-
-exports.createCustomer = (req, res) => {
-  db.connect()
-  // get new customer data from req.body
-  const newCustomer = req.body
-  // query to create a customer...
-  db.query(`INSERT INTO customers VALUES (null, "${newCustomer.first_name}",
-  "${newCustomer.last_name}", "${newCustomer.phone}", "${newCustomer.email}");`,
-  (err, results) => {
-    // if error return error with status 500
-      if(err) {
-        res.status(500).send(err)
+//delete out the js after dbconfig
+exports.getCustomersById = (req, res) => {
+    db.connect()
+    const { byId } = req.params
+    db.query(`SELECT * from customers WHERE id = ${ byId }`, (err, rows) => { 
+        if (err) {
+            res.status(500).send(err)
         return
-      }
-      // otherwise return OK with status 201
-      res.status(201).send('Customer created')
+        }
+        res.send(rows)
     })
-  db.end()
-}
+     db.end()
+   }
+    
+
+// exports.createCustomer = (req, res) => {
+//   db.connect()
+  // get new customer data from req.body
+  // const newCustomer = req.body
+  // query to create a customer...
+  // db.query(`INSERT INTO customers VALUES (null, "${newCustomer.first_name}",
+  // "${newCustomer.last_name}", "${newCustomer.phone}", "${newCustomer.email}");`,
+  // (err, results) => {
+    // if error return error with status 500
+      // if(err) {
+      //   res.status(500).send(err)
+      //   return
+      // }
+      // otherwise return OK with status 201
+//       res.status(201).send('Customer created')
+//     })
+//   db.end()
+// }
