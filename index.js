2f4d460f659343c6a67ccf74f1b102fb83f25a08
Todd Albert <todd@bocacode.com>
2021-07-14 10:27:13 -0400

    added get.customers route and function; not yet working

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const { getCustomers } = require('./src/customers')
 
 const app = express()
 
 // PUT ROUTES HERE...
-
+app.get('/customers', getCustomers)
 
 app.listen(3000, () => {
   console.log('listening on http://localhost:3000...')
--- src/customers.js
+++ src/customers.js
@@ -1,1 +1,4 @@
 // PUT CUSTOMER-RELATED FUNCTIONS HERE...
+exports.getCustomers = (req, res) => {
+  res.send('customers will appear here...')
+}
