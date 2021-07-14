0c1d1225039c65530abf3bdb36ed5b4210e73f11
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-07-14 16:09:14 -0400

    first try merging custById

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { getCustomers, createCustomer } = require('./src/customers')
+const { getCustomersById, createCustomer, getCustomers } = require('./src/customers')
 
 const app = express()
 app.use(express.json())
 
 // PUT ROUTES HERE...
+app.get('/customers/:byId', getCustomersById)
+
 app.get('/customers', getCustomers)
 
 app.post('/customers', createCustomer)
--- src/customers.js
+++ src/customers.js
@@ -1,28 +1,26 @@
-// const mysql = require('mysql')
-// const { dbconfig } = require('../dbconfig')
-
-// const db = mysql.createConnection(dbconfig)
-
-// // PUT CUSTOMER-RELATED FUNCTIONS HERE...
-// exports.getCustomers = (req, res) => {
-//   db.connect()
-//   db.query(`SELECT customers.*, pets.name, pets.type, pets.size
-//     FROM customers LEFT JOIN pets ON pets.customer_id = customers.id`, (err, rows) => {
-//     if (err) {
-//       res.status(500).send(err)
-//       return
-//     }
-//     res.send(rows)
-//   })
-//   db.end()
-// }
-
 const mysql = require('mysql')
 const { dbconfig } = require('../dbconfig')
 
-const db = mysql.createConnection(dbconfig)
-//delete out the js after dbconfig
+
+// PUT CUSTOMER-RELATED FUNCTIONS HERE...
+exports.getCustomers = (req, res) => {
+  const db = mysql.createConnection(dbconfig)
+  db.connect()
+  db.query(`SELECT * FROM customers`, (err, rows) => {
+    if (err) {
+      res.status(500).send(err)
+      return
+    }
+    res.send(rows)
+  })
+  db.end()
+}
+
+
+
+
 exports.getCustomersById = (req, res) => {
+    const db = mysql.createConnection(dbconfig)
     db.connect()
     const { byId } = req.params
     db.query(`SELECT * from customers WHERE id = ${ byId }`, (err, rows) => { 
@@ -36,21 +34,21 @@ exports.getCustomersById = (req, res) => {
    }
     
 
-// exports.createCustomer = (req, res) => {
-//   db.connect()
+exports.createCustomer = (req, res) => {
+  const db = mysql.createConnection(dbconfig)
+  db.connect()
   // get new customer data from req.body
-  // const newCustomer = req.body
+  const newCustomer = req.body
   // query to create a customer...
-  // db.query(`INSERT INTO customers VALUES (null, "${newCustomer.first_name}",
-  // "${newCustomer.last_name}", "${newCustomer.phone}", "${newCustomer.email}");`,
-  // (err, results) => {
-    // if error return error with status 500
-      // if(err) {
-      //   res.status(500).send(err)
-      //   return
-      // }
+  db.query(`INSERT INTO customers VALUES (null, "${newCustomer.first_name}",
+  "${newCustomer.last_name}", "${newCustomer.phone}", "${newCustomer.email}");`,
+  (err, results) => {
+      if(err) {
+        res.status(500).send(err)
+        return
+      }
       // otherwise return OK with status 201
-//       res.status(201).send('Customer created')
-//     })
-//   db.end()
-// }
+      res.status(201).send('Customer created')
+    })
+  db.end()
+}
