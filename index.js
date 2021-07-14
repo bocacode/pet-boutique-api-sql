132032f97aafe9152dcf8bc981b1c10d2b4b86c4
chrisdeleonn <cdizzel@ymail.com>
2021-07-14 15:36:56 -0400

    revised changes n3

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-<<<<<<< HEAD
 const express = require("express")
 const {
   getCustomers,
   createCustomer,
   deleteCustomer,
 } = require("./src/customers")
-=======
-const express = require('express')
-const { getCustomers, createCustomer } = require('./src/customers')
-const { getPetById, getPets } = require('./src/pets')
->>>>>>> main
+const { getPetById, getPets } = require("./src/pets")
 
 const app = express()
 app.use(express.json())
 
 // PUT ROUTES HERE...
-<<<<<<< HEAD
-=======
-app.get('/customers', getCustomers)
-app.get('/pets/:byId', getPetById)
-app.get('/pets', getPets)
-
->>>>>>> main
+app.get("/customers", getCustomers)
+app.get("/pets/:byId", getPetById)
+app.get("/pets", getPets)
 
 app.delete("/customers/:id", deleteCustomer)
-
-app.get("/customers", getCustomers)
 
 app.post("/customers", createCustomer)
 
 app.listen(3000, () => {
-<<<<<<< HEAD
   console.log("listening on http://localhost:3000...")
 })
-=======
-  console.log('listening on http://localhost:3000...')
-})
-
-
-
-
->>>>>>> main
