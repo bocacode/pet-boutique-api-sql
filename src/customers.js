dcb73eedd4b5b5edec3a81f901140a662b01d33c
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-07-14 16:43:10 -0400

    fixed pull request issues

--- src/customers.js
+++ src/customers.js
@@ -1,24 +1,23 @@
 const mysql = require("mysql")
 const { dbconfig } = require("./dbconfig")
 
-
 // PUT CUSTOMER-RELATED FUNCTIONS HERE...
 exports.getCustomers = (req, res) => {
   const db = mysql.createConnection(dbconfig)
   db.connect()
-  db.query(`SELECT * FROM customers`, (err, rows) => {
-    if (err) {
-      res.status(500).send(err)
-      return
+  db.query(
+    `SELECT customers.* , pets.name, pets.type, pets.size
+    FROM customers LEFT JOIN pets ON pets.customer_id = customers.id`,
+    (err, rows) => {
+      if (err) {
+        res.status(500).send(err)
+        return
       }
       res.send(rows)
     }
   )
   db.end()
 }
-
-
-
 
 exports.getCustomersById = (req, res) => {
     const db = mysql.createConnection(dbconfig)
@@ -34,8 +33,7 @@ exports.getCustomersById = (req, res) => {
      db.end()
    }
     
-
-exports.createCustomer = (req, res) => {
+  exports.createCustomer = (req, res) => {
   const db = mysql.createConnection(dbconfig)
   db.connect()
   // get new customer data from req.body
